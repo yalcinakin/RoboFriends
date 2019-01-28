2978e7c38d91d06d5caa13b8c91b782d0c512f8d
YALCIN AKIN <yalcinakin@sabanciuniv.edu>
2019-01-28 16:54:19 +0900

    Updates

---
+++ precache-manifest.c9000e89449b349b72bddf8b3ede0043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "22bfa01eac16ec30c384",
+    "url": "/RoboFriends/static/js/main.22bfa01e.chunk.js"
+  },
+  {
+    "revision": "6bfdd8a36295e543472c",
+    "url": "/RoboFriends/static/js/1.6bfdd8a3.chunk.js"
+  },
+  {
+    "revision": "22bfa01eac16ec30c384",
+    "url": "/RoboFriends/static/css/main.0c233824.chunk.css"
+  },
+  {
+    "revision": "6bfdd8a36295e543472c",
+    "url": "/RoboFriends/static/css/1.2747cbb4.chunk.css"
+  },
+  {
+    "revision": "798c4f5b2c319f604fd88834423ab6f7",
+    "url": "/RoboFriends/index.html"
+  }
+];
