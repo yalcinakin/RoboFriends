0ac65cf22d658136dc55301889be7ac281d499f7
YALCIN AKIN <yalcinakin@sabanciuniv.edu>
2019-02-21 11:13:11 +0900

    Error Boundary and Redux is added to the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,52 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components//Scroll';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import {setSearchField, requestRobots} from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ""
-    }
-  }
 
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({robots: users}));
-  }
-
-  onSearchChange = (event) => {
-    this.setState({searchField: event.target.value});
+    this.props.onRequestRobots();
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const {searchField, onSearchChange, robots, isPending} = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    if (!this.state.robots.length){
+    if (isPending){
       return <h1>Loading...</h1>
     }
     else{
       return (
         <div className= "tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots}/>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
           </Scroll>
         </div>
 
@@ -46,4 +55,22 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// constructor() {
+//   super()
+//   this.state = {
+//     searchField: '',
+//     robots: []
+//   }
+// }
+
+// componentDidMount(){
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => response.json())
+//   .then(users => this.setState({robots: users}));
+// }
+
+// onSearchChange = (event) => {
+//   this.setState({searchField: event.target.value});
+// }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
+import {searchRobots, requestRobots} from './reducers';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(<Provider store={store} >
+                  <App />
+                </Provider>
     ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
